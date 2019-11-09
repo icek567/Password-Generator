dc2c3ab9665cbf966bd9f240dc3b4aaafcbdcd66
Sergio Perez <perezsergio97.sp@gmail.com>
2019-11-09 17:52:29 -0600

    Updating homework progress

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+
 function generate(){
-    
+   
+    let complexity = document.getElementById("slider").value;
+
+
+  
    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$%";
     
-   let password 
+   let password = "";
+
+
+   for( var i = 0; i <= complexity; i++ ){
+       password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1 )));
+
+   }
+
+   document.getElementById("display").value = password;
 
 }
